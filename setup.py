947324d24882c73ab769bcdb1fc918fea086c0a7
Greg "GothAck" Miell <greg@gothack.ninja>
2020-09-02 21:03:28 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="acmeasync",
-    version="0.1.1a1",
+    version="0.1.2a",
     description="ACME v2 Client using asyncio",
     author='Greg "GothAck " Miell',
     author_email="acmeasync@greg.gothack.ninja",
